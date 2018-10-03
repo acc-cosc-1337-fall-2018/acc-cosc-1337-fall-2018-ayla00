d01965bc8a2a5265d8ec3709e5c0ccffde2f2c81
ayla00 <42852966+ayla00@users.noreply.github.com>
2018-10-02 20:15:23 -0500

    Update main.cpp

--- assignments/05-classes/main.cpp
+++ assignments/05-classes/main.cpp
@@ -6,9 +6,9 @@ using std::cout;
 int main()
 {
 	Invoice invoice;
-	invoice.add_invoice_detail(InvoiceDetail(10, 10);
-	invoice.add_invoice_detail(InvoiceDetail(5, 5);
-	invoice.add_invoice_detail(InvoiceDetail(100, 2);
+	invoice.add_invoice_detail(InvoiceDetail(10, 10));
+	invoice.add_invoice_detail(InvoiceDetail(5, 5));
+	invoice.add_invoice_detail(InvoiceDetail(100, 2));
 
 	std::cout << invoice.get_total();
 
