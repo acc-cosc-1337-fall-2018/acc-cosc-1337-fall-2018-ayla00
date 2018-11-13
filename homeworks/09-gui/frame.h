c5e28410c5091c8c9e27d2ec1903eb322d835dfd
ayla00 <42852966+ayla00@users.noreply.github.com>
2018-11-13 15:14:10 -0600

    Update frame.h

--- homeworks/09-gui/frame.h
+++ homeworks/09-gui/frame.h
@@ -1,18 +1,12 @@
 #ifndef FRAME_H
 #define FRAME_H
-#include <wx/wx.h>
-#include "panel.h"
+#include<wx/wx.h>
 
 class Frame : public wxFrame
 {
-public: 
+public:
 	Frame();
-
 private:
-	void OnHello(wxCommandEvent& event);
-	void OnExit(wxCommandEvent& event);
-	void OnAbout(wxCommandEvent& event);
-
 };
 
-#endif //!FRAME_H
+#endif // !FRAME_H
