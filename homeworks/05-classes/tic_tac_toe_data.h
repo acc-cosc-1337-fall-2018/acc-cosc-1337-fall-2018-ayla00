3de400cb8ed1907fc8b351eb45b092ad32302438
5960606 <5960606@HLC2411-09>
2018-11-26 20:28:52 -0600

    kjfds;l

--- homeworks/05-classes/tic_tac_toe_data.h
+++ homeworks/05-classes/tic_tac_toe_data.h
@@ -14,12 +14,12 @@
 class TicTacToeData
 {
 public:
-	TicTacToeData() = default;
+	//TicTacToeData() = default;
 	std::vector<std::unique_ptr<TicTacToe>> get_games();
 	void save_pegs(std::vector<Peg>& pegs);
 
 private:
-	std::string file_name{ "tic_tac_toe.dat" };  //check this as it doesn't look right
+	std::string file_name{ "tic_tac_toe.dat" };  
 	std::vector<std::string> pegs;
 };
 
